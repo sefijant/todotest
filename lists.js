0fef10e74a9774fc0d0ab5bed020a8fc4254a20e
Josefi <Josefi@DESKTOP-D37K14T>
2018-03-22 10:40:00 +0200

    some useless changes

--- lists.js
+++ lists.js
@@ -1,16 +1,16 @@
 
 var app = angular.module("ToDo", []);
 app.controller("MainController", function ($scope, $http) {
-	$http.get('/Lists/travel').then(function(response) {
+	this.$http.get('/Lists/travel').then(function(response) {
         $scope.travelLists = response.data;
     });
-	$http.get('/Lists/army').then(function(response) {
+	this.$http.get('/Lists/army').then(function(response) {
         $scope.armyLists = response.data;
     });
-	$http.get('/Lists/shopping').then(function(response) {
+	this.$http.get('/Lists/shopping').then(function(response) {
         $scope.shoppingLists = response.data;
     });
-	$http.get('/Lists/other').then(function(response) {
+	this.$http.get('/Lists/other').then(function(response) {
         $scope.otherLists = response.data;
     });
 	var categories = [{ "name": "קניות", "image": ".\\resources\\shopping.jpg", "modal": "#shopping" }, { "name": "צבא", "image": ".\\resources\\army.jpg", "modal": "#army" }, { "name": "טיולים", "image": ".\\resources\\travel.jpg", "modal": "#travel" }, { "name": "אחר", "image": ".\\resources\\else.jpg", "modal": "#other" }];
